faef375de7fca81c2bd4589f84a073cb5a6a33c7
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-10-03 03:22:04 +0700

    init project

---
+++ Kotlin/SpringBoot-Cloud-Kafka-Manual-Acknowledge/src/main/kotlin/com/tirmizee/SpringBootCloudKafkaManualAcknowledge/SpringBootCloudKafkaManualAcknowledgeApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee.SpringBootCloudKafkaManualAcknowledge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootCloudKafkaManualAcknowledgeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootCloudKafkaManualAcknowledgeApplication>(*args)
+}
---
+++ Kotlin/SpringBoot-Cloud-Kafka-Manual-Acknowledge/src/test/kotlin/com/tirmizee/SpringBootCloudKafkaManualAcknowledge/SpringBootCloudKafkaManualAcknowledgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootCloudKafkaManualAcknowledge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootCloudKafkaManualAcknowledgeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
