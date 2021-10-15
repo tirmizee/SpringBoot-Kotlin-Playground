b478cd20333875b271355cee8d66276ba32c6068
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-10-16 03:23:04 +0700

    update application.yaml

--- SpringBoot-Redis-ReactiveTemplate/src/main/kotlin/com/tirmizee/SpringBootRedisReactiveTemplateApplication.kt
+++ SpringBoot-Redis-ReactiveTemplate/src/main/kotlin/com/tirmizee/SpringBootRedisReactiveTemplateApplication.kt
@@ -1,10 +1,33 @@
 package com.tirmizee
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootRedisReactiveTemplateApplication
+class SpringBootRedisReactiveTemplateApplication : CommandLineRunner {
+
+	@Value("\${test.service-ip}")
+	lateinit var serviceIp: String
+
+	@Value("\${test.service-port}")
+	lateinit var servicePort: String
+
+	@Value("\${test.protocol}")
+	lateinit var protocol: String
+
+	@Value("\${test.service-address}")
+	lateinit var serviceAddress: String
+
+	override fun run(vararg args: String?) {
+		println("servicePort $servicePort")
+		println("serviceIp $serviceIp")
+		println("protocol $protocol")
+		println("serviceAddress $serviceAddress")
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringBootRedisReactiveTemplateApplication>(*args)
