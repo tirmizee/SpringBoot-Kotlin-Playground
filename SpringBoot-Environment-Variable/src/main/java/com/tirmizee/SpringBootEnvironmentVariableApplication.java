c0b98b701821576379c4448e0aebb50f284de9ce
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-10-16 04:14:14 +0700

    init project

---
+++ SpringBoot-Environment-Variable/src/main/java/com/tirmizee/SpringBootEnvironmentVariableApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEnvironmentVariableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEnvironmentVariableApplication.class, args);
+	}
+
+}
