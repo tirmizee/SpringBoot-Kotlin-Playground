d0b1793f180152fa101e5c31715e8acca9e7e1f1
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-10-20 02:59:01 +0700

    add deployment.yaml

--- SpringBoot-K8s-Secret-Mount-File/src/main/kotlin/com/tirmizee/SpringBootK8sSecretMountFileApplication.kt
+++ SpringBoot-K8s-Secret-Mount-File/src/main/kotlin/com/tirmizee/SpringBootK8sSecretMountFileApplication.kt
@@ -1,10 +1,35 @@
 package com.tirmizee
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+import org.springframework.util.FileCopyUtils
+import java.io.FileNotFoundException
+import java.io.IOException
+
 
 @SpringBootApplication
-class SpringBootK8sSecretMountFileApplication
+class SpringBootK8sSecretMountFileApplication : CommandLineRunner {
+
+	@Value("file:/config/config-1.json")
+	lateinit var resource1: Resource
+
+	@Value("file:/config/config-2.properties")
+	lateinit var resource2: Resource
+
+	override fun run(vararg args: String?) {
+		try {
+			println("resource1 ${this.resource1.filename} ${this.resource1.contentLength()}")
+			println("resource2 ${this.resource2.filename} ${this.resource2.contentLength()}")
+		} catch (e: FileNotFoundException) {
+			println("FileNotFoundException")
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringBootK8sSecretMountFileApplication>(*args)
