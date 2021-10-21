801a8efaea40fd947382abf0e0723854a5522bb1
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-10-22 04:16:55 +0700

    init project

---
+++ SpringBoot-K8s-Volume-PVC/src/main/kotlin/com/tirmizee/SpringBootK8sVolumePVC/SpringBootK8sVolumePvcApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee.SpringBootK8sVolumePVC
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootK8sVolumePvcApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootK8sVolumePvcApplication>(*args)
+}
---
+++ SpringBoot-K8s-Volume-PVC/src/test/kotlin/com/tirmizee/SpringBootK8sVolumePVC/SpringBootK8sVolumePvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootK8sVolumePVC
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootK8sVolumePvcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
