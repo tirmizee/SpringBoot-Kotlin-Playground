4d8c259ab3001b12f286d5c4219127592908bbff
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-10-22 23:33:46 +0700

    print memory log

--- SpringBoot-K8s-Deployment/src/main/kotlin/com/tirmizee/SpringBootK8sDeploymentApplication.kt
+++ SpringBoot-K8s-Deployment/src/main/kotlin/com/tirmizee/SpringBootK8sDeploymentApplication.kt
@@ -1,10 +1,17 @@
 package com.tirmizee
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootK8sDeploymentApplication
+class SpringBootK8sDeploymentApplication : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		println("MAX MEMORY ${Runtime.getRuntime().maxMemory()}")
+		println("FREE MEMORY ${Runtime.getRuntime().freeMemory()}")
+		println("USE MEMORY ${Runtime.getRuntime().maxMemory() - Runtime.getRuntime().freeMemory()}")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringBootK8sDeploymentApplication>(*args)
