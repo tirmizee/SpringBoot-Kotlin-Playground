1301d8118ac30ffdd2f86f4896078c2b9e7b4883
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-10-22 16:23:29 +0700

    update .k8s

--- SpringBoot-K8s-Volume-PVC/src/main/kotlin/com/tirmizee/SpringBootK8sVolumePvcApplication.kt
+++ SpringBoot-K8s-Volume-PVC/src/main/kotlin/com/tirmizee/SpringBootK8sVolumePvcApplication.kt
@@ -1,10 +1,46 @@
 package com.tirmizee
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.*
 
 @SpringBootApplication
-class SpringBootK8sVolumePvcApplication
+class SpringBootK8sVolumePvcApplication: CommandLineRunner {
+
+	override fun run(vararg args: String?) {
+		val path: Path = Paths.get("/data/${UUID.randomUUID()}.txt")
+		runCatching {
+			Files.createFile(path)
+		}.onSuccess {
+			println("File ${path.fileName} created.")
+		}.onFailure {
+			println("File ${path.fileName} already exists.")
+		}
+
+		var pathnames: Array<String>
+		val f = File("/data")
+		f.runCatching {
+
+			pathnames = list()
+
+			for (pathname in pathnames) {
+				println(pathname)
+			}
+
+		}.onSuccess {
+			println("Read file success.")
+		}.onFailure {
+			println("Read file failure.")
+		}
+
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringBootK8sVolumePvcApplication>(*args)
