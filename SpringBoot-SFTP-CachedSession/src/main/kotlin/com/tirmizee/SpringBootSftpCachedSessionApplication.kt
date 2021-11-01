f7c8162900a45e893451f0317a4bd2ed1a458c2a
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-11-02 03:08:50 +0700

    init project

---
+++ SpringBoot-SFTP-CachedSession/src/main/kotlin/com/tirmizee/SpringBootSftpCachedSessionApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootSftpCachedSessionApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootSftpCachedSessionApplication>(*args)
+}
