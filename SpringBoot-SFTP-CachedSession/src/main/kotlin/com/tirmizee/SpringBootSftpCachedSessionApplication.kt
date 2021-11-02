424421726c5db64cf9a8e9e9b4a11651b631183c
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-11-03 01:39:31 +0700

    revise code

--- SpringBoot-SFTP-CachedSession/src/main/kotlin/com/tirmizee/SpringBootSftpCachedSessionApplication.kt
+++ SpringBoot-SFTP-CachedSession/src/main/kotlin/com/tirmizee/SpringBootSftpCachedSessionApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.integration.sftp.session.SftpRemoteFileTemplate
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
