67ae1b056590b3f21be798649111f78afe5196a1
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-11-03 04:42:38 +0700

    init project

---
+++ SpringBoot-SFTP-InboundChannel-DSL/src/main/kotlin/com/tirmizee/SpringBootSFTPInboundChannelDSL/SpringBootSftpInboundChannelDslApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee.SpringBootSFTPInboundChannelDSL
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootSftpInboundChannelDslApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootSftpInboundChannelDslApplication>(*args)
+}
---
+++ SpringBoot-SFTP-InboundChannel-DSL/src/test/kotlin/com/tirmizee/SpringBootSFTPInboundChannelDSL/SpringBootSftpInboundChannelDslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootSFTPInboundChannelDSL
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootSftpInboundChannelDslApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
