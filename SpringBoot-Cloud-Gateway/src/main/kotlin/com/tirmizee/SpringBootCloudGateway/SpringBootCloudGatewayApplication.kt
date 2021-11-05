258a3c114be78f9ee3edb6fff7f33b98f5136267
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-11-06 06:21:58 +0700

    init project

---
+++ SpringBoot-Cloud-Gateway/src/main/kotlin/com/tirmizee/SpringBootCloudGateway/SpringBootCloudGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee.SpringBootCloudGateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootCloudGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootCloudGatewayApplication>(*args)
+}
---
+++ SpringBoot-Cloud-Gateway/src/test/kotlin/com/tirmizee/SpringBootCloudGateway/SpringBootCloudGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootCloudGateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootCloudGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
