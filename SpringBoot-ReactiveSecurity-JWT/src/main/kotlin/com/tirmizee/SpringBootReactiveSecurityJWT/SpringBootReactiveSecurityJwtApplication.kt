0cec30ebbf2631be03d40084dec17fb4f5321523
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-11-07 03:50:51 +0700

    init project

---
+++ SpringBoot-ReactiveSecurity-JWT/src/main/kotlin/com/tirmizee/SpringBootReactiveSecurityJWT/SpringBootReactiveSecurityJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee.SpringBootReactiveSecurityJWT
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootReactiveSecurityJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootReactiveSecurityJwtApplication>(*args)
+}
---
+++ SpringBoot-ReactiveSecurity-JWT/src/test/kotlin/com/tirmizee/SpringBootReactiveSecurityJWT/SpringBootReactiveSecurityJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootReactiveSecurityJWT
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootReactiveSecurityJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
