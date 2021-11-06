e258117a59f4e0aae7eceaee7c501022847151e3
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-11-07 00:04:35 +0700

    init project

---
+++ SpringBoot-WebClient-LogFilter/src/main/kotlin/com/tirmizee/SpringBootWebClientLogFilter/SpringBootWebClientLogFilterApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee.SpringBootWebClientLogFilter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootWebClientLogFilterApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootWebClientLogFilterApplication>(*args)
+}
---
+++ SpringBoot-WebClient-LogFilter/src/test/kotlin/com/tirmizee/SpringBootWebClientLogFilter/SpringBootWebClientLogFilterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootWebClientLogFilter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootWebClientLogFilterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
