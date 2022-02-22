6da1233a04ed2ad1c637b18bdbf757fdbda9f5a9
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-02-22 08:38:57 +0700

    init new project

---
+++ SpringBoot-WebFilter/src/main/kotlin/com/tirmizee/SpringBootWebFilterApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootWebFilterApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootWebFilterApplication>(*args)
+}
