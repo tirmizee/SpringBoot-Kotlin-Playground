278b28b4602ef42813583f8af3646f5f6ea16fff
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-03-02 01:41:45 +0700

    init project

---
+++ SpringBoot-Modify-Request/src/main/kotlin/com/tirmizee/SpringBootModifyRequest/SpringBootModifyRequestApplication.kt
@@ -0,0 +1,11 @@
+package com.tirmizee.SpringBootModifyRequest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootModifyRequestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootModifyRequestApplication>(*args)
+}
---
+++ SpringBoot-Modify-Request/src/test/kotlin/com/tirmizee/SpringBootModifyRequest/SpringBootModifyRequestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootModifyRequest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootModifyRequestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
