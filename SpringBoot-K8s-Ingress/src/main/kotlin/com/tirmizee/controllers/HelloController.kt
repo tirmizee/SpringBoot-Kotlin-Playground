98836ca80ef815133b6a1e3caa78970f41147910
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-05-14 02:21:22 +0700

    update project

--- SpringBoot-K8s-Ingress/src/main/kotlin/com/tirmizee/controllers/HelloController.kt
+++ SpringBoot-K8s-Ingress/src/main/kotlin/com/tirmizee/controllers/HelloController.kt
@@ -1,4 +1,25 @@
 package com.tirmizee.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HelloController {
+
+    @GetMapping("/")
+    suspend fun main(): String? {
+        return "path /"
+    }
+
+    @GetMapping("/members")
+    suspend fun members(): String? {
+        return "path /members"
+    }
+
+    @GetMapping("/members/{member}")
+    suspend fun membersBy(@PathVariable member: String): String? {
+        return "path /members/$member"
+    }
+
 }
